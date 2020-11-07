afa7d26d0e226350e4336e2b42eeeab21d316243
Chepman32 <53965045+Chepman32@users.noreply.github.com>
2020-11-07 14:50:44 +0300

    DayItem

--- components/DayItem.js
+++ components/DayItem.js
@@ -1,9 +1,91 @@
 import React from "react"
-import { Text, View } from "react-native"
-export const DayItem = ({temp}) => {
+import { ScrollView, StyleSheet, Text, View } from "react-native"
+export const DayItem = ({nameOfDay, temp, feelsLike, city, description, wind, sunRise, sunSet,
+   temp_min, temp_max, humidity, pressure}) => {
   return (
-    <View>
-      <Text>{temp} </Text>
+    <View style={styles.container}>
+      <Text style={styles.nameOfDay} >{nameOfDay && nameOfDay} </Text>
+      <Text style={styles.temp} onPress={() => console.log(feelsLike)}>{temp}°</Text>
+      <View style={styles.rowContainer}>
+      <Text style={styles.temp_info} >Ощущается как: {feelsLike}°</Text>
+      </View>
+      <Text style={styles.city} >{city} </Text>
+      <Text style={styles.description} >{description}</Text>
+      <ScrollView>
+      <Text style={styles.info} >Скорость ветра: 
+      <Text style={styles.temp_info} >{wind}м/с </Text>
+      </Text>
+        <Text style={styles.info} >Восход: 
+        &nbsp;<Text style={styles.temp_info} >{sunRise}</Text> </Text>
+        <Text style={styles.info} >Закат:
+        &nbsp;<Text style={styles.temp_info} >{sunSet}</Text>
+         </Text>
+        <Text style={styles.info} >
+          Минимальная температура воздуха:
+          <Text style={styles.temp_info} >{temp_min}°</Text>
+        </Text>
+        <Text style={styles.info} >
+        Максимальная температура воздуха:
+          <Text style={styles.temp_info} >{temp_max}°</Text>
+        </Text>
+      <Text style={styles.info} >
+        Влажность: <Text style={styles.temp_info} >{humidity}%</Text>
+       </Text>
+       <Text style={styles.info} >
+        Давление: <Text style={styles.temp_info} >{pressure} мм. рт. ст.</Text>
+       </Text>
+      </ScrollView>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center"
+  },
+  city: {
+    marginTop: 50,
+    fontSize: 40,
+    fontWeight: "600",
+    lineHeight: 40,
+    color: "#E7C689"
+  },
+  temp: {
+    marginTop: 20,
+    fontSize: 120,
+    fontWeight: "300",
+    color: "tomato"
+  },
+  temp_info: {
+    fontSize: 30,
+    fontWeight: "600",
+    color: "tomato"
+  },
+  nameOfDay: {
+    marginTop: 20,
+    fontSize: 50,
+    fontWeight: "400",
+    color: "#fff"
+  },
+  description: {
+    marginTop: 50,
+    marginBottom: 30,
+    fontSize: 40,
+    fontWeight: "600",
+    lineHeight: 40,
+    color: "#E7C689"
+  },
+  row: {
+    flexDirection: "row",
+  },
+  info: {
+    justifyContent: "flex-end",
+    marginTop: 20,
+    marginBottom: 20,
+    fontSize: 30,
+    fontWeight: "400",
+    lineHeight: 40,
+    textAlign: "center",
+    color: "#789DD9"
+  }
+});
--- components/Main.js
+++ components/Main.js
@@ -12,6 +12,8 @@ export default function LocationComp({ setStatus, setPeriod }) {
   const [errorMsg, setErrorMsg] = useState(null);
   const [temp, setTemp] = useState("")
   const [feelsLike, setFeelsLike] = useState("")
+  const [temp_min, setTemp_min] = useState("")
+  const [temp_max, setTemp_max] = useState("")
   const [description, setDescription] = useState("")
   const [items, setItems] = useState([])
   const [nameOfDay, setNameOfDay] = useState("")
@@ -20,6 +22,10 @@ export default function LocationComp({ setStatus, setPeriod }) {
   const [sunSet, setSunSet] = useState("")
   const [sunRise, setSunRise] = useState("")
   const [dayTime, setDayTime] = useState("")
+  const [humidity, setHumidity] = useState("")
+  const [pressure, setPressure] = useState("")
+  const [dayId, setDayId] = useState("")
+  const [dayItems, setDayItems] = useState([])
 
   useEffect(() => {
     (async () => {
@@ -50,20 +56,24 @@ useEffect(() => {
     await fetch(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&&units=metric&&lang=ru&appid=9b8df7b7f2207d424cdfdef63a988822`);
     let responseJson = await response.json();
-    if(location) {
-      setLatitude(latitude => location.coords.latitude)
+      if(location) {
+        setLatitude(latitude => location.coords.latitude)
     setLongitude(longitude => location.coords.longitude)
     setDescription(responseJson.weather[0].description)
     setCity(responseJson.name)
     setTemp(Math.ceil(responseJson.main.temp))
     setFeelsLike(responseJson.main.feels_like)
+    setTemp_min(responseJson.main.temp_min)
+    setTemp_max(responseJson.main.temp_max)
     setSunRise(unixConverter(responseJson.sys.sunrise))
     setSunSet(unixConverter(responseJson.sys.sunset))
     setWind(responseJson.wind.speed)
-    console.log(responseJson)
     setDayTime(responseJson.weather[0].icon[responseJson.weather[0].icon.length - 1] === "n" ? "night" : "day")
-    }
-  } catch (error) {
+    setHumidity(responseJson.main.humidity)
+    setPressure(responseJson.main.pressure * 0.75)
+      }
+  } 
+  catch (error) {
     console.error(error);
   }
 } 
@@ -74,32 +84,45 @@ async function get5DaysData() {
       `http://api.openweathermap.org/data/2.5/forecast?q=Moscow&units=metric&appid=2e356aefbd53857048281362b509db4a`);
     let responseJson = await response.json();
     if(location) {
-      let array = responseJson.list.slice(0, 1)
-    getNameOfDay(array)
-    setItems(responseJson.list.map(i => <DayItem temp={i.main.temp} />))
+      let array = responseJson.list
+      getNameOfDay(array)
+    for(let i = 0; i < array.length / 8; i++) {
+      array = array.slice(i * 8,(i + 1) * 16)
+      console.log(array)
+      setDayItems(array.map(a => <DayItem temp={a.main.temp} feelsLike={a.main.feels_like} />))
+    }
     }
   } catch (error) {
     console.error(error);
   }
 }
+
 const getNameOfDay = (array) => {
   let dateObj = new Date()
   let dayName = dateObj.toLocaleString("default", {weekday: "long"})
-  setNameOfDay(dayName)
+  setNameOfDay(nameOfDay)
   switch(dayName) {
     case "Понедельник":
       array[0].dayName = "Вторник"
+      setDayId(2)
       case "Вторник":
+        setDayId(3)
       array[0].dayName = "Среда"
       case "Среда":
+        setDayId(4)
       array[0].dayName = "Четверг"
+      setDayId(5)
       case "Четверг" :
+        setDayId(5)
       array[0].dayName = "Пятница"
       case "Пятница" :
+        setDayId(6)
       array[0].dayName = "суббота"
       case "Суббота" :
+        setDayId(7)
       array[0].dayName = "Воскресенье"
       case "Воскресенье" :
+        setDayId(1)
       array[0].dayName = "Понедельник"
       default:
         array[0].dayName = "Вторник"
@@ -108,18 +131,38 @@ const getNameOfDay = (array) => {
   return (
     <View style={styles.container}>
       <Text style={styles.nameOfDay} >{nameOfDay && nameOfDay} </Text>
-      <Text style={styles.temp} onPress={() => console.log(feelsLike)}>{location && temp}°</Text>
+      <Text style={styles.temp} onPress={() => console.log(description)}>{location && temp}°</Text>
       <View style={styles.rowContainer}>
-      <Text style={{...styles.temp, fontSize: 30, fontWeight: "600"}} >Ощущается как: {location && feelsLike}°</Text>
+      <Text style={styles.temp_info} >Ощущается как: {location && feelsLike}°</Text>
       </View>
       <Text style={styles.city} >{city} </Text>
       <Text style={styles.description} >{location && description}</Text>
       <ScrollView>
-      <View style={styles.rowContainer}>
-      <Text style={styles.info} >Скорость ветра: {wind}м/с </Text>
-        <Text style={styles.info} >Восход: {sunRise} </Text>
-        <Text style={styles.info} >Закат: {sunSet} </Text>
-      </View>
+      <Text style={styles.info} >Скорость ветра: 
+      <Text style={styles.temp_info} >{wind}м/с </Text>
+      </Text>
+        <Text style={styles.info} >Восход: 
+        &nbsp;<Text style={styles.temp_info} >{sunRise}</Text> </Text>
+        <Text style={styles.info} >Закат:
+        &nbsp;<Text style={styles.temp_info} >{sunSet}</Text>
+         </Text>
+        <Text style={styles.info} >
+          Минимальная температура воздуха:
+          <Text style={styles.temp_info} >{location && temp_min}°</Text>
+        </Text>
+        <Text style={styles.info} >
+        Максимальная температура воздуха:
+          <Text style={styles.temp_info} >{location && temp_max}°</Text>
+        </Text>
+      <Text style={styles.info} >
+        Влажность: <Text style={styles.temp_info} >{humidity}%</Text>
+       </Text>
+       <Text style={styles.info} >
+        Давление: <Text style={styles.temp_info} >{pressure} мм. рт. ст.</Text>
+       </Text>
+       {
+         dayItems
+       }
       </ScrollView>
     </View>
   );
@@ -132,26 +175,30 @@ const styles = StyleSheet.create({
   },
   city: {
     marginTop: 50,
-    marginBottom: 30,
     fontSize: 40,
     fontWeight: "600",
     lineHeight: 40,
     color: "#E7C689"
   },
   temp: {
-    marginTop: 50,
+    marginTop: 20,
     fontSize: 120,
     fontWeight: "300",
     color: "tomato"
   },
+  temp_info: {
+    fontSize: 30,
+    fontWeight: "600",
+    color: "tomato"
+  },
   nameOfDay: {
-    marginTop: 50,
+    marginTop: 20,
     fontSize: 50,
     fontWeight: "400",
     color: "#fff"
   },
   description: {
-    marginTop: 100,
+    marginTop: 50,
     marginBottom: 30,
     fontSize: 40,
     fontWeight: "600",
@@ -162,11 +209,13 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
   info: {
+    justifyContent: "flex-end",
     marginTop: 20,
     marginBottom: 20,
     fontSize: 30,
     fontWeight: "400",
     lineHeight: 40,
+    textAlign: "center",
     color: "#789DD9"
   }
 });
